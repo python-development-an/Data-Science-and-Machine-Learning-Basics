50ce2549341be92720a8f86c40bde1395b60a512
Anjali pandey <127732287+coderanju@users.noreply.github.com>
2025-10-01 20:08:21 +0545

    day1 learing including with installation

---
+++ d1.py
@@ -0,0 +1,26 @@
+"""In this we will learn about the very basis of python ,starting with installation of python IDE and now at first step we will learn about
+commenting system, for a single line we can just use # if we wnat to use for multiple line we can use ("""""")for commenting.
+
+*VARIABLES:they are use to store things in python 
+     RULE:
+     1.YOU CANT USE NUMBER AT VRIABLE START.
+     2.YOU CANT USE SPACES IN VARIABLE
+     3.YOU SHOULDNT USE SPECIAL CHARACTERS IN VARIABLES.
+
+
+     @ NAMING CONVECTION:
+     1.CAMELCASE.
+     2.PASCAL CASE.
+     SNAKE CASE.
+
+     DATATYPES:Things we store in variables and it defines what data types variables are.python hs bulit in data types for different kinds of data.
+
+     $TYPES OF  DATATYPES:
+     1> NUMBER:INT,FLOAT AND COMPLEX.
+     2>STRING
+     3>BOOLEAN"""
+
+a="Anjali"
+print(type(a))
+
+#it will print as string 
